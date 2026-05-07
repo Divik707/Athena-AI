ebbfc14f009300b488a1370f5354cb5f634290f6
divik <diviksaxenaa2610@gmail.com>
2026-05-07 16:10:44 +0530

    re-init frontend

--- frontend/src/config.ts
+++
@@ -1,3 +0,0 @@
-export const VITE_SUPABASE_URL="https://qlbnzezoodsbcyqunflk.supabase.co"
-export const VITE_SUPABASE_PUBLISHABLE_KEY="sb_publishable_xe8TVTTfu7z1qYrSgiL3jQ_IWRtAvyo"
-export const BACKEND_URL="http://localhost:3001"
