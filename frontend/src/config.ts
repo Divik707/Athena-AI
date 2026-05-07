5b662529b6d1c82ed94eb395d51bde0194ed3fbf
divik <diviksaxenaa2610@gmail.com>
2026-05-07 16:01:12 +0530

    connected frontend auth via superbase to postgres database

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,3 @@
 export const VITE_SUPABASE_URL="https://qlbnzezoodsbcyqunflk.supabase.co"
 export const VITE_SUPABASE_PUBLISHABLE_KEY="sb_publishable_xe8TVTTfu7z1qYrSgiL3jQ_IWRtAvyo"
+export const BACKEND_URL="http://localhost:3001"
--- frontend/src/pages/Conversation.tsx
+++ frontend/src/pages/Conversation.tsx
@@ -1,35 +1,84 @@
-import React from 'react';
-import { createClient, User } from '@supabase/supabase-js'
-import { VITE_SUPABASE_PUBLISHABLE_KEY, VITE_SUPABASE_URL } from '@/config';
-import { useEffect, useState } from 'react';
-import {useNavigate } from 'react-router-dom';
+import axios from "axios";
+import { createClient, User } from "@supabase/supabase-js";
+import {
+  BACKEND_URL,
+  VITE_SUPABASE_PUBLISHABLE_KEY,
+  VITE_SUPABASE_URL,
+} from "@/config";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const superbase = createClient(VITE_SUPABASE_URL, VITE_SUPABASE_PUBLISHABLE_KEY)
+const supabase = createClient(
+  VITE_SUPABASE_URL,
+  VITE_SUPABASE_PUBLISHABLE_KEY
+);
 
 export const Conversation = () => {
-  const naviagte = useNavigate();
-  const [user, setUser] = useState<User | null>();
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState<User | null>(null);
+
   useEffect(() => {
     async function conversations() {
-      const { data, error } = await superbase.auth.getUser();
-      if(data.user) {
-        setUser(data.user);
-      }
-      if(error) {
-        alert("SORRY! Athena could not get you old conversations")
+      try {
+        const { data, error } = await supabase.auth.getSession();
+
+        if (error) {
+          alert("Could not get session");
+          return;
+        }
+
+        const session = data.session;
+
+        if (!session) {
+          return;
+        }
+
+        setUser(session.user);
+
+        const token = session.access_token;
+
+        const resp = await axios.get(
+          `${BACKEND_URL}/Athena/conversations`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        console.log("getting axios here--------------------------------------------------")
+        console.log(resp.data);
+      } catch (err) {
+        console.log(err);
       }
     }
-    conversations();
-  }, [])
 
-  const logout = () => {
-    superbase.auth.signOut();
+    conversations();
+  }, []);
+
+  const logout = async () => {
+    await supabase.auth.signOut();
+
     setUser(null);
-    naviagte('/')
-  }
+
+    navigate("/");
+  };
+
   return (
     <div>
-      {!user?.email ? <button onClick={() => naviagte('/auth')}>login</button> : <div>{user.email} <button onClick={() => logout()}>logout</button></div>}
+      {!user ? (
+        <button onClick={() => navigate("/auth")}>
+          Login
+        </button>
+      ) : (
+        <div>
+          {user.email}
+
+          <button onClick={logout}>
+            Logout
+          </button>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
